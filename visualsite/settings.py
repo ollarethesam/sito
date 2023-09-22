2f696db2a0ee24ed0cee1579fa5794c221408013
BuildTools <unconfigured@null.spigotmc.org>
2023-09-22 11:42:56 +0200

    second commit

--- visualsite/settings.py
+++ visualsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--*8)f(e2zrd1_!jmiyxnej7rlu*p^jr9&w(d*2%7ixcrs_arlu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
