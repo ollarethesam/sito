e336b3aff7a5d45feed2bc268f64f797d51ead28
BuildTools <unconfigured@null.spigotmc.org>
2023-09-22 17:07:25 +0200

    7 commit

--- visualsite/settings.py
+++ visualsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--*8)f(e2zrd1_!jmiyxnej7rlu*p^jr9&w(d*2%7ixcrs_arlu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
